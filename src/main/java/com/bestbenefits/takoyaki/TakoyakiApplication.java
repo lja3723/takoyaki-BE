7521c744caf1e74713b67df54976fc4cf75674da
coco3x <inferior3x@gmail.com>
2023-11-28 00:45:02 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/bestbenefits/takoyaki/TakoyakiApplication.java
@@ -0,0 +1,13 @@
+package com.bestbenefits.takoyaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakoyakiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakoyakiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestbenefits/takoyaki/Takoyaki1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestbenefits.takoyaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takoyaki1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
