664aaca2a69537b8ef567f079a27195a412d6a64
coco3x <inferior3x@gmail.com>
2023-12-01 20:11:45 +0900

    add: UserService 추가
    아직 아무것도 없지만 곧 메서드 추가할 예정

---
+++ src/main/java/com/bestbenefits/takoyaki/service/UserService.java
@@ -0,0 +1,16 @@
+package com.bestbenefits.takoyaki.service;
+
+import com.bestbenefits.takoyaki.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    //sign up
+
+    //login
+
+}
