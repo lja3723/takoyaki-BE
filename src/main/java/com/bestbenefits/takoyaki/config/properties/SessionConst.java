22c6fc2224e5918432332f59700a8eafbb92cbb1
coco3x <inferior3x@gmail.com>
2023-12-06 23:18:28 +0900

    add: Session에 저장될 속성들의 이름을 상수로 만듦
    하드코딩을 지양하면 좋지 않을까요?^^

---
+++ src/main/java/com/bestbenefits/takoyaki/config/properties/SessionConst.java
@@ -0,0 +1,6 @@
+package com.bestbenefits.takoyaki.config.properties;
+
+public class SessionConst {
+    public static final String ID = "id";
+    public static final String AUTHENTICATION = "authentication";
+}
