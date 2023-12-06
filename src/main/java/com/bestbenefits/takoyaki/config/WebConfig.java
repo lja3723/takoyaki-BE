a183decf642aeeec55a0922cbfa97569b2aca81c
coco3x <inferior3x@gmail.com>
2023-12-06 23:59:08 +0900

    add: WebMvcConfigurer 구현
    로그인 인증 확인하는 인터셉터랑 세션 속성의 값 가져오는 아규먼트 리졸버 추가하기 위해

---
+++ src/main/java/com/bestbenefits/takoyaki/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.bestbenefits.takoyaki.config;
+
+import com.bestbenefits.takoyaki.config.annotation.SessionMethodArgumentResolver;
+import com.bestbenefits.takoyaki.interceptor.AuthenticationCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthenticationCheckInterceptor())
+                .addPathPatterns("/user/**")
+                .excludePathPatterns(
+                        "/js/**", "/oauth_example", "/oauth", "/favicon.ico", //실험용이니 나중에 삭제하기
+                        "/user/login-check",
+                        "/user/oauth/login-url/**",
+                        "/user/oauth/login/**",
+                        "/user/duplicate-nickname",
+                        "/user/oauth/login/additional-info"
+                );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SessionMethodArgumentResolver());
+    }
+}
