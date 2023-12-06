d52f4bf54df2a89af2a8f318b65863c65e878c82
coco3x <inferior3x@gmail.com>
2023-12-06 22:25:25 +0900

    fix: character set 바꿈
    이모지 저장하려고 mysql utf8mb4로 바꿈
    참고: 필요해서 ddl-auto랑 세션 timeout 값 바꿨음 별 의미 없음

--- src/main/java/com/bestbenefits/takoyaki/config/properties/oauth/OAuthKakaoConst.java
+++ src/main/java/com/bestbenefits/takoyaki/config/properties/oauth/OAuthKakaoConst.java
@@ -9,20 +9,18 @@ public final class OAuthKakaoConfig {
     public static final String AUTHORIZATION_CODE_URI = "/oauth/authorize";
     public static final String ACCESS_TOKEN_URI = "/oauth/token";
     public static final String REDIRECT_URI = "http://localhost:8080/oauth";
-    //사용자 동의 항목
     public static final String AGREEMENT_URI = "/v2/user/scopes";
     public static final String USER_INFO_URI = "/v2/user/me";
     public static final String LOGOUT_URI = "/v1/user/logout";
     public static final String CLIENT_ID = "832c674221e0bd9d5f7a07f5c3ba5d85";
-    public static final String AUTHORIZATION_CODE_URL = URL1 +
+    public static final String LOGIN_URL = URL1 +
                     UriComponentsBuilder.newInstance()
                         .path(OAuthKakaoConfig.AUTHORIZATION_CODE_URI)
                         .queryParam("response_type", "code")
                         .queryParam("client_id", CLIENT_ID)
                         .queryParam("redirect_uri", OAuthKakaoConfig.REDIRECT_URI)
                         .queryParam("prompt", "select_account")
-                        .build(true)
-                        .toString();
+                        .build(true);
 
     private OAuthKakaoConfig(){}
 }
