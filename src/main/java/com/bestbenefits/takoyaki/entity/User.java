496235588679b8936a7c95ed57f0bb02efa00778
coco3x <inferior3x@gmail.com>
2023-12-28 17:29:24 +0900

    fix: User 엔티티 수정
    -접근 제어자 설정
    -길이 설정
    -닉네임 업데이트 날짜 추가

--- src/main/java/com/bestbenefits/takoyaki/entity/User.java
+++ src/main/java/com/bestbenefits/takoyaki/entity/User.java
@@ -1,35 +1,38 @@
 package com.bestbenefits.takoyaki.entity;
 
 import com.bestbenefits.takoyaki.config.properties.oauth.OAuthSocialType;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(length = 16, unique = true)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(length = 100, nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     @Enumerated(value = EnumType.STRING)
     private OAuthSocialType social;
 
-    @Column(name = "created_at")
-    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private LocalDate nicknameUpdatedAt;
+
+    @Column
     private LocalDateTime createdAt;
 
     @Builder
@@ -37,10 +40,14 @@ public class User {
         this.email = email;
         this.social = social;
         this.createdAt = LocalDateTime.now();
+        this.nicknameUpdatedAt = LocalDate.now().minusDays(1);
     }
 
     public void updateNickname(String nickname){
         this.nickname = nickname;
+    }
+    public void updateNicknameUpdatedAt(){
+        this.nicknameUpdatedAt = LocalDate.now();
     }
 
 }
