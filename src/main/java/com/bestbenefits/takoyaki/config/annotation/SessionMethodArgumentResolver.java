ec22eacc71b745a4179a0fbf4abf8cbca95b154a
coco3x <inferior3x@gmail.com>
2024-01-04 23:37:46 +0900

    comment: TODO 추가

--- src/main/java/com/bestbenefits/takoyaki/config/annotation/SessionMethodArgumentResolver.java
+++ src/main/java/com/bestbenefits/takoyaki/config/annotation/SessionMethodArgumentResolver.java
@@ -2,7 +2,12 @@ package com.bestbenefits.takoyaki.config.annotation;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -11,7 +16,6 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import java.util.Optional;
 
 public class SessionMethodArgumentResolver implements HandlerMethodArgumentResolver {
-
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(Session.class);
@@ -22,8 +26,8 @@ public class SessionMethodArgumentResolver implements HandlerMethodArgumentResol
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         HttpSession session = request.getSession();
         String attribute = parameter.getParameterAnnotation(Session.class).attribute();
-
-        return Optional.ofNullable(session.getAttribute(attribute))
-                .orElseThrow(() -> new NullPointerException("Invalid session attribute"));
+            return Optional.ofNullable(session.getAttribute(attribute))
+                    .orElseThrow(() -> new NullPointerException("Invalid session attribute"));
+        //TODO: 로그인 안되어있어도 세션 예외가 발생하지 않도록 임시 조치하기
     }
 }
